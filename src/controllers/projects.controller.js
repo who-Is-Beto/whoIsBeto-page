c66077eb7340a55ba1d330a2edd516c0006eaf89
Beto Cortés <roberto.cortes13233@gmail.com>
2021-02-10 17:11:25 -0600

    project card

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -2,9 +2,6 @@ import views from '../views/projects.html'
 
 export default () => {
 
-  const homeIcon = document.getElementById('PROJECTS__ICON')
-  homeIcon.setAttribute("class", "nav__item-active")
-
   const divElement = document.createElement('main')
   divElement.setAttribute("id", "projects")
 
