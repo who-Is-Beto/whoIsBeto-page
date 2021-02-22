f98eae62fdd13c12c6c9f1294cd9372cd62aa819
Beto Cortés <roberto.cortes13233@gmail.com>
2021-02-22 17:22:05 -0600

    Mobile first done... I guess

--- src/controllers/contact.controller.js
+++ src/controllers/contact.controller.js
@@ -5,6 +5,18 @@ export default () => {
   const divElement = document.createElement('main')
   divElement.setAttribute("id", "contact")
 
+
   divElement.innerHTML = views
+  const $form = divElement.querySelector('#form')
+
+  function handleSubmitEmail(event) {
+    event.preventDefault()
+
+    const formData = new FormData(this)
+    trucazo.setAttribute('href', `mailto:whoisbeto@gmail.com?=${formData.get('name')}${formData.get('email')}&body=${formData.get('message')}`)
+    trucazo.click()
+  }
+
+  $form.addEventListener('submit', handleSubmitEmail)
   return divElement
 }
